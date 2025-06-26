29be462742b388a864930871eff1841d426b1d41
WAS4YH <was4yh@jp.bosch.com>
2025-06-26 02:25:22 -0400

    use html page

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route("/emotionDetector", methods=['POST'])
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
 def emotion_detector_endpoint():
-    data = request.get_json()
-    if not data:
-        return jsonify({"error": "No JSON data received"}), 400
-    text_to_analyse = data.get('text', '')
+    text_to_analyse = request.args.get('textToAnalyze')
+    if not text_to_analyse:
+        return jsonify({"error": "No TextToAnalyze arg received"}), 400
     emotion_dict = emotion_detector(text_to_analyse)
     return f"""
 For the given statement, the system response is 'anger': {emotion_dict['anger']}, 
 'disgust': {emotion_dict['disgust']}, 'fear': {emotion_dict['fear']}, 'joy': {emotion_dict['joy']} 
 and 'sadness': {emotion_dict['sadness']}. The dominant emotion is {emotion_dict['dominant_emotion']}
 """
+
 
 if __name__ == '__main__':
     app.run(debug=True)
